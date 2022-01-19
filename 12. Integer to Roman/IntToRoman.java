edefb2c7c7ee1d817cac8cc61e498cbe4af6fd61
Gauri Khodaskar <gkhodaskar@gmail.com>
2022-01-19 11:09:19 -0800

    Create IntToRoman.java
    Problem Description: https://leetcode.com/problems/integer-to-roman/

---
+++ 12. Integer to Roman/IntToRoman.java
@@ -0,0 +1,36 @@
+// Method I inputs
+private static final int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+private static final String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+
+// Method II inputs
+// private static final String[] thousands = {"", "M", "MM", "MMM"};
+// private static final String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}; 
+// private static final String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+// private static final String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+
+public String intToRoman(int num) {
+
+    // Method I
+    // TC: O(1), Finite set of roman numerals
+    // SC: O(1)
+    if (num <= 0)
+    return null;
+
+     StringBuilder sb = new StringBuilder();
+    // Loop through each symbol, stopping if num becomes 0.
+    for (int i = 0; i < values.length && num > 0; i++) {
+        // Repeat while the current symbol still fits into num.
+        while (values[i] <= num) {
+            num -= values[i];
+            sb.append(symbols[i]);
+        }
+    }
+    return sb.toString();
+
+    // Method II: Hardcode digits
+    // TC: O(1), Finite set of roman numerals
+    // SC: O(1)
+    // return thousands[num / 1000] + hundreds[num % 1000 / 100] + tens[num % 100 / 10] + ones[num % 10];
+}
