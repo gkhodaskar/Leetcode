7b8f1101635b989c649ba4a35a011da080fd2c83
Gauri Khodaskar <gkhodaskar@gmail.com>
2022-01-19 10:58:08 -0800

    Create MaxArea.java
    Problem Description: https://leetcode.com/problems/container-with-most-water/

---
+++ 11. Container With Most Water/MaxArea.java
@@ -0,0 +1,21 @@
+public int maxArea(int[] height) {
+    int left = 0, right = height.length - 1;
+    int max = 0; // Maximum area
+  
+    // Two Pointer, Single pass
+    // TC: O(n), SC: O(1)
+  
+    while(left < right){
+        int area = Math.min(height[left], height[right]) * (right - left);
+        max = Math.max(max,area); // Update max
+      
+        // Move the pointer with shorter line
+        if(height[left] > height[right]){
+            right--;
+        } else {
+            left++;
+        }
+    }
+
+    return max;
+}
